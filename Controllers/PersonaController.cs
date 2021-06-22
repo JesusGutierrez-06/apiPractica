fcd59c9db91599885958f5fb738c52193efd2508
JesusGutierrez <jesusgutierrez0106@gmail.com>
2021-06-21 22:49:35 -0400

    add funciones de controlador persona

--- Controllers/PersonaController.cs
+++ Controllers/PersonaController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using apiPractica.Context;
+using apiPractica.Models;
 
 namespace apiPractica.Controllers
 {
@@ -20,10 +21,43 @@ namespace apiPractica.Controllers
         [HttpGet]
         public ActionResult GetAll()
         {
-            return Ok(context.persona.ToList());
+            try
+            {
+                return Ok(context.persona.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-
+        [HttpGet("{idpersona}", Name ="GetById")]
+        public ActionResult GetById(int id)
+        {
+            try
+            {
+                var persona = context.persona.FirstOrDefault(item => item.idpersona == id);
+                return Ok(persona);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody] Persona persona)
+        {
+            try
+            {
+                context.persona.Add(persona);
+                context.SaveChanges();
+                return CreatedAtRoute("GetById", new { persona.idpersona }, persona);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
 
     }
